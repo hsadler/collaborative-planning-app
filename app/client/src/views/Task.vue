b8660badbe273313df714c0bf23dab908c116be1
Harry Sadler <harrypatricksadler@gmail.com>
2020-03-10 17:51:50 -0700

    wip /task route implementation

--- app/client/src/views/Task.vue
+++ app/client/src/views/Task.vue
@@ -3,7 +3,20 @@
 
 		<h1>Task: <span class="task-title">{{getTaskTitle}}</span></h1>
 
-		<!-- TODO: votes will go here -->
+		<table class="vote-variants">
+			<tr>
+				<th>Complexity</th>
+				<th>Votes</th>
+			</tr>
+			<tr 
+				v-for="(data, variant) in getVariantToData" 
+				:key="variant"
+				@click="submitVote(variant)"
+			>
+				<td>{{variant}}</td>
+				<td>{{data.voteCount}}</td>
+			</tr>
+		</table>
 
 	</div>
 </template>
@@ -20,18 +33,58 @@ export default {
 		return {
 			httpService: services.use('httpService'),
 			socketService: services.use('socketService'),
+			userService: services.use('userService'),
 			task: null,
 			voteVariants: [],
 			votes: []
 		}
 	},
-	methods: {},
+	methods: {
+		submitVote(variant) {
+			var params = {
+				user_id: this.userService.getUser().uuid4,
+				task_id: this.task.uuid4,
+				vote_variant: variant
+			}
+			this.httpService.get(
+				'/api/create-or-update-vote', 
+				params
+			).then((res) => {
+				if(res.success) {
+					console.log('vote cast:')
+					console.log(res.vote)
+				}
+			})
+		},
+		getVotesFromVariant(variant) {
+			if(variant.voteValue) {
+				return variant.voteValue
+			} else {
+				return ''
+			}
+		}
+	},
 	computed: {
 		getTaskTitle() {
 			if(this.task) {
 				return this.task.title
 			}
 			return ''
+		},
+		getVariantToData() {
+			var result = {}
+			this.voteVariants.forEach((voteVariant) => {
+				result[voteVariant.variant] = {
+					'voteVariant': voteVariant,
+					'votes': [],
+					'voteCount': 0
+				}
+			})
+			this.votes.forEach((vote) => {
+				result[vote.variant]['votes'].push(vote)
+				result[vote.variant]['voteCount'] += 1
+			})
+			return result
 		}
 	},
 	created () {
@@ -47,21 +100,17 @@ export default {
 		// fetch vote variants
 		this.httpService.get('/api/get-available-vote-variants').then((res) => {
 			if(res.success) {
-				console.log(res.vote_variants)
 				this.voteVariants = res.vote_variants
-			}
-		})
-		// fetch votes
-		params = {
-			task_id: this.taskId
-		}
-		this.httpService.get(
-			'/api/get-all-votes-by-task', 
-			params
-		).then((res) => {
-			if(res.success) {
-				console.log(res.votes)
-				this.votes = res.votes
+				// fetch votes
+				params = { task_id: this.taskId }
+				this.httpService.get(
+					'/api/get-all-votes-by-task', 
+					params
+				).then((res) => {
+					if(res.success) {
+						this.votes = res.votes
+					}
+				})		
 			}
 		})
 	}
@@ -74,6 +123,21 @@ export default {
 			span.task-title {
 				color: #42b983;	
 			}
+		}
+		table {
+			border-collapse: collapse;
+			width: 100%;
+		}
+		td, th {
+			border: 1px solid #dddddd;
+			text-align: left;
+			padding: 8px;
+		}
+		td {
+			cursor: pointer;
+		}
+		tr:nth-child(even) {
+			background-color: #dddddd;
 		}
 	}
 </style>
